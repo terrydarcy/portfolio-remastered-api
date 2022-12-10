e9c08c41cfa06daa72d20a73ffed2e6442c62ca0
Terry D'Arcy <terrykdarcy@gmail.com>
2022-12-10 15:21:23 +0000

    fixed server issue and added aws certification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,14 @@
 // https://medium.com/weekly-webtips/how-to-use-an-express-router-within-the-cloud-function-developing-environment-cb64face4043
 // https://node-postgres.com/guides/async-express
 //TODO: do not use for production
-require('dotenv').config();
-const cors = require("cors");
+require("dotenv").config();
+const cors = require("cors")({
+  origin: true,
+});
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors);
 
 const githubRoutes = require("./routes/github");
 const statusRoutes = require("./routes/status");
@@ -15,6 +17,6 @@ const bookingRoutes = require("./routes/booking");
 // routings
 app.use("/github", githubRoutes);
 app.use("/status", statusRoutes);
-app.use("/booking", bookingRoutes);
+// app.use("/booking", bookingRoutes);
 
 exports.portfolio_remastered = functions.https.onRequest(app);
